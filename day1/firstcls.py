c8a3438f72d76b000f393d56d6543dcaf3f0e2e4
vibhuti-rathi <85829191+vibhuti-rathi@users.noreply.github.com>
2021-06-15 22:37:28 +0530

    Update and rename first.py to firstcls.py

---
+++ day1/firstcls.py
@@ -0,0 +1,13 @@
+print("hello World")
+name=input("enter the name")
+age=int(input("enter the age"))
+print(name,age)
+
+#int , float and string data type
+a=12
+b=12.5
+c="amita"
+
+print(type(a))
+print(type(c))
+
